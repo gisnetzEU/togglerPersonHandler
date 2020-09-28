337135eeece32b72f83a4d2c0074bc3d091c72de
GisnetzEU <gismontero.mail@gmail.com>
2020-09-28 16:57:42 +0200

    Updates

---
+++ precache-manifest.94f76036b16aef830149cfc5ea77ec58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5a501d622d4b12ab7e4c73e62eca51",
+    "url": "/togglerPersonHandler/index.html"
+  },
+  {
+    "revision": "f5b13563f0b926b44f75",
+    "url": "/togglerPersonHandler/static/css/main.d957a0c0.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/togglerPersonHandler/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/togglerPersonHandler/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b13563f0b926b44f75",
+    "url": "/togglerPersonHandler/static/js/main.2f70f607.chunk.js"
+  },
+  {
+    "revision": "87f1da7c15aebba95459",
+    "url": "/togglerPersonHandler/static/js/runtime-main.f41bf0dd.js"
+  }
+]);
